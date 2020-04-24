41742b234a4bd722893356a2aa1ed6675900295e
Fayçal BORDJAH <fbordjah@linagora.com>
2020-04-24 15:12:18 +0200

    enhance

--- src/ipssihelp/worker/urls.py
+++ src/ipssihelp/worker/urls.py
@@ -8,10 +8,11 @@ urlpatterns = [
     path('demand', views.demand, name='demand'),
     path('supply', views.supply, name='supply'),
     path('detail/<slug:slug>/', views.detail, name='detail'),
-    path('account/ads/', views.getAllAds, name='ads'),
+    path('account/ads/', views.get_all_ads, name='ads'),
     path('account/signup/', views.signup, name='signup'),
     path('account/login/', views.login_worker, name='login'),
     path('account/logout/', views.logout_worker, name='logout'),
     path('account/profile/', views.worker_profile, name='profile'),
     path('account/contact/<slug:slug>/', views.contact, name='contact'),
+    path('account/messages/', views.get_all_messages, name='messages'),
 ]
